54bd7fa38e562fefd46a74ee3c190c62f033e61e
Alicia <alicia.magro@ouidou.fr>
2025-06-10 16:33:59 +0200

    Create form for the events creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,22 @@ end
 def show
   @event = Event.find(params[:id])
 end
+
+def new
+  @event = Event.new
+end
+
+def create
+  @event = current_user.created_events.build(event_params)
+
+  if @event.save
+    redirect_to @event, notice: "Event successfully created !"
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+  def event_params
+    params.expect(event: [ :name, :date, :description ])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [ :show ]
 
-  resources :events
+  resources :events, only: [ :index, :show, :new, :create ]
 end
