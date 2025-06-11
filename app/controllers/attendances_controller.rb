691abf10f9b58cd30efe413b6001b38d41b6e59a
Alicia <alicia.magro@ouidou.fr>
2025-06-11 10:07:06 +0200

    Update the view

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,21 @@
+class AttendancesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @attendance = current_user.attendances.build(attendance_params)
+
+    if @attendance.save
+      flash[:notice] = "You're now part of the event ! 🎉"
+    else
+      flash[:alert] = @attendance.errors.full_messages.to_sentence
+    end
+
+    redirect_to @attendance.event
+  end
+
+  private
+
+  def attendance_params
+    params.permit(:event_id)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 def show
   @user = User.find(params[:id])
+  @events = @user.created_events
+  @attended_events = @user.attended_events
 end
 end
